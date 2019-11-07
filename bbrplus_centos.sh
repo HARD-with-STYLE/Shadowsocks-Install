e710f9b6a2014fff2c00ee73f4f9c0172915a2ad
Yuk1n0 <39364985+Yuk1n0@users.noreply.github.com>
2019-11-07 18:32:28 +0800

    Minor improvements

--- bbrplus_centos.sh
+++ bbrplus_centos.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 #
-# Original Author:  cx9208   Licensed: GPLv3
-# Thanks:
-# @cx9208  <https://github.com/cx9208>
+# Original Author:  cx9208 <https://github.com/cx9208>  Licensed: GPLv3
+# Copyright (C) 2019 Yuk1n0
 
 kernel_version="4.14.151"
 if [[ ! -f /etc/redhat-release ]]; then
-	echo -e "Only support centos..."
+	echo -e "Only support Centos..."
 	exit 0
 fi
 
@@ -15,13 +14,15 @@ if [[ "$(uname -r)" == "${kernel_version}" ]]; then
 	exit 0
 fi
 
-echo -e "Uninstalling lotServer..."
+echo -e "Checking lotServer..."
 sed -i '/net.core.default_qdisc/d' /etc/sysctl.conf
 sed -i '/net.ipv4.tcp_congestion_control/d' /etc/sysctl.conf
-if [[ -e /appex/bin/serverSpeeder.sh ]]; then
+if [[ -e /appex/bin/lotServer.sh ]]; then
+	echo -e "Uninstalling lotServer..."
 	wget --no-check-certificate -O appex.sh https://raw.githubusercontent.com/MoeClub/lotServer/master/Install.sh && chmod +x appex.sh && bash appex.sh uninstall
 	rm -f appex.sh
 fi
+
 echo -e "Downloading Kernel..."
 wget --no-check-certificate https://github.com/Yuk1n0/Shadowsocks-Install/raw/master/Centos7/x86_64/kernel-${kernel_version}.rpm
 echo -e "Installing Kernel..."
@@ -41,11 +42,18 @@ grub2-set-default 'CentOS Linux (${kernel_version}) 7 (Core)'
 echo -e "Enable bbr module..."
 echo "net.core.default_qdisc=fq" >>/etc/sysctl.conf
 echo "net.ipv4.tcp_congestion_control=bbrplus" >>/etc/sysctl.conf
+sysctl -p
 rm -f kernel-${kernel_version}.rpm
 
-read -p "bbrplus installation completed，reboot server now ? [Y/n] :" answer
-[ -z "${answer}" ] && answer="y"
-if [[ $answer == [Yy] ]]; then
-	echo -e "Rebooting..."
-	reboot
-fi
+while true; do
+	read -p "bbrplus installation completed，reboot server now ? [Y/n] :" answer
+	[ -z "${answer}" ] && answer="y"
+	if [[ $answer == [Yy] ]]; then
+		echo -e "Rebooting..."
+		break
+	else
+		echo -e "Error, you should choose [Y/n] !"
+		echo
+	fi
+done
+reboot
