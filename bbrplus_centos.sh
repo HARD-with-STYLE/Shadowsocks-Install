5af65a0b53966eaefa963ada97df39cd7fd50f4a
Yuk1n0 <39364985+Yuk1n0@users.noreply.github.com>
2019-11-29 11:01:03 +0800

    Optimizate bbrplus_centos.sh

--- bbrplus_centos.sh
+++ bbrplus_centos.sh
@@ -1,31 +1,37 @@
 #!/usr/bin/env bash
 #
-# Original Author:  cx9208 <https://github.com/cx9208>  Licensed: GPLv3
-# Copyright (C) 2019 Yuk1n0
+# Original Author: cx9208 <https://github.com/cx9208>  Licensed: GPLv3
+# Copyright (C) 2019-2020 Yuk1n0
 
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+plain='\033[0m'
 kernel_version="4.14.154"
+
 if [[ ! -f /etc/redhat-release ]]; then
-	echo -e "Only support Centos..."
+	echo -e "[${red}Error${plain}] Only support Centos..."
 	exit 0
 fi
 
 if [[ "$(uname -r)" == "${kernel_version}" ]]; then
-	echo -e "Kernel has been installed..."
+	echo -e "[${yellow}Warning${plain}] bbrplus kernel has been installed..."
 	exit 0
 fi
 
-echo -e "Checking lotServer..."
+echo -e "[${green}Info${plain}] Checking lotServer..."
 sed -i '/net.core.default_qdisc/d' /etc/sysctl.conf
 sed -i '/net.ipv4.tcp_congestion_control/d' /etc/sysctl.conf
 if [[ -e /appex/bin/lotServer.sh ]]; then
-	echo -e "Uninstalling lotServer..."
+	echo -e "[${green}Info${plain}] Uninstalling lotServer..."
 	wget --no-check-certificate -O appex.sh https://raw.githubusercontent.com/MoeClub/lotServer/master/Install.sh && chmod +x appex.sh && bash appex.sh uninstall
 	rm -f appex.sh
 fi
+echo -e "[${green}Info${plain}] Checking lotServer comlete..."
 
-echo -e "Downloading Kernel..."
+echo -e "[${green}Info${plain}] Downloading bbrplus kernel..."
 wget --no-check-certificate https://github.com/Yuk1n0/Shadowsocks-Install/raw/master/Centos7/x86_64/kernel-${kernel_version}.rpm
-echo -e "Installing Kernel..."
+echo -e "[${green}Info${plain}] Installing bbrplus kernel..."
 yum install -y kernel-${kernel_version}.rpm
 
 #Check
@@ -33,13 +39,14 @@ list="$(awk -F\' '$1=="menuentry " {print i++ " : " $2}' /etc/grub2.cfg)"
 target="CentOS Linux (${kernel_version})"
 result=$(echo $list | grep "${target}")
 if [[ "$result" == "" ]]; then
-	echo -e "Failed to install bbrplus..."
+	echo -e "[${red}Error${plain}] Failed to install bbrplus kernel..."
 	exit 1
 fi
+echo -e "[${green}Info${plain}] Installing bbrplus kernel complete..."
 
-echo -e "Switching to new bbrplus-kernel..."
+echo -e "[${green}Info${plain}] Switching to new bbrplus kernel..."
 grub2-set-default 'CentOS Linux (${kernel_version}) 7 (Core)'
-echo -e "Enable bbr module..."
+echo -e "[${green}Info${plain}] Enable bbr module..."
 echo "net.core.default_qdisc=fq" >>/etc/sysctl.conf
 echo "net.ipv4.tcp_congestion_control=bbrplus" >>/etc/sysctl.conf
 rm -f kernel-${kernel_version}.rpm
@@ -48,10 +55,10 @@ while true; do
 	read -p "bbrplus installation completed，reboot server now ? [Y/n] :" answer
 	[ -z "${answer}" ] && answer="y"
 	if [[ $answer == [Yy] ]]; then
-		echo -e "Rebooting..."
+		echo -e "[${green}Info${plain}] Rebooting..."
 		break
 	else
-		echo -e "Error, you should choose [Y/n] !"
+		echo -e "[${red}Error${plain}] Please enter [Y/n] !"
 		echo
 	fi
 done
